d864025a8069b341be52cebc0b75cefba5e57ee3
keshav goyal <keshavgoyal2014@gmail.com>
2019-01-12 22:37:54 +0530

    added effect to mobile nav

--- moblienavtimeout.js
+++ moblienavtimeout.js
@@ -1,4 +1,20 @@
 setTimeout(function () {
 var timeout1 = document.querySelector('.timeout');
-timeout1.classList.toggle('timeout2');
+timeout1.classList.add('timeout2');
 },2300);
+
+var x=0;
+
+
+document.addEventListener('scroll',function () {
+    var timeout1 = document.querySelector('.timeout');
+    if(window.scrollY>=x)
+    {
+        timeout1.classList.remove('timeout2');
+    }
+    else
+    {
+        timeout1.classList.add('timeout2');
+    }
+    x=window.scrollY;
+    })
