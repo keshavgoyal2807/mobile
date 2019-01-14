aeca51ffff9727b3448473f732a43c9c4a7a98da
keshav goyal <keshavgoyal2014@gmail.com>
2019-01-14 22:19:55 +0530

    final changes

--- moblienavtimeout.js
+++ moblienavtimeout.js
@@ -22,9 +22,13 @@ document.addEventListener('scroll',function () {
 var login = document.querySelector('.login');
 var login1 = document.querySelector('.login1');
 var login2 = document.querySelector('.login2');
+var p1 = document.querySelector('.p1');
+var p2 = document.querySelector('.p2');
 login.addEventListener('click',function () {
     console.log('ada');
     login2.classList.toggle('login1add');
     login1.classList.toggle('login1add');
+    p2.classList.toggle('padd');
+    p1.classList.toggle('padd');
     login.classList.toggle('loginadd');
 });
